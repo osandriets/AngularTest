062e5621f1934a14aad43e2652674bc62d9c3777
OSA <osandriiets@gmail.com>
2017-08-19 21:08:14 +0300

    add style

--- src/app/views/sevise/sevise.component.ts
+++ src/app/views/sevise/sevise.component.ts
@@ -13,107 +13,107 @@ export class SeviseComponent implements OnInit {
     {
       id: 1,
       name: 'Accountancy',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 2,
       name: 'Associations',
-      img: 'assets/images/services/associations.svg'
+      img: 'assets/images/service/associations.svg'
     },
     {
       id: 3,
       name: 'Buy/Sell Dental Practice',
-      img: 'assets/images/services/buy.svg'
+      img: 'assets/images/service/buy.svg'
     },
     {
       id: 4,
       name: 'Clinical Waste Collection',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 5,
       name: 'Computer Service',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 6,
-      name: 'Life\n' + 'Support Training',
-      img: 'assets/images/services/accountancy.svg'
+      name: 'CPR/Basic Life\n' + 'Support Training jjhkbkb khikh',
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 7,
       name: 'Dental Recruitment',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 8,
       name: 'Dental Software',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 9,
       name: 'Electrical Safety\n' + 'And Testing',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 10,
       name: 'Marketing',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 11,
       name: 'Others',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 12,
       name: 'Repairs',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 13,
-      name: 'Solicitors',
-      img: 'assets/images/services/accountancy.svg'
+      name: 'Solicitors hhhhhhhhhh  ',
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 14,
       name: 'Surgery Design',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 15,
       name: 'Training & Courses',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 16,
       name: 'Web Designers',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 17,
       name: 'Cleaning Agenciese',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 18,
-      name: 'Dental  Chair Recovery',
-      img: 'assets/images/services/accountancy.svg'
+      name: 'Dental  Chair Recovery hhhhhhhhh',
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 19,
       name: 'Equipment Engineering',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 20,
       name: 'Selling Gold',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     },
     {
       id: 21,
       name: 'Uniform & Workwea',
-      img: 'assets/images/services/accountancy.svg'
+      img: 'assets/images/service/accountancy.svg'
     }
   ];
 
